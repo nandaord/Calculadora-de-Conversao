bb809b90ebb5e2650f1eee294d266dd325c3246a
Maria Fernanda Ordonho <142419456+nandaord@users.noreply.github.com>
2024-08-28 11:38:20 -0300

    Create calculadora.c
    commit inicial

---
+++ calculadora.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:35)\n1.a(Não feito)\n1.b(Não feito)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
+  return 0;
+}
