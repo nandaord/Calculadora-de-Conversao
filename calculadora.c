f3081db76824ea624db018e675fc4537bbd3ac21
Maria Fernanda Ordonho <142419456+nandaord@users.noreply.github.com>
2024-08-28 11:56:45 -0300

    Update calculadora.c
    commit pra adicionar a conversão de um número de base 10 para base 2 (binário)

--- calculadora.c
+++ calculadora.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int conv(int resp, int opcao) {
+    int tamanho = (int)(log2(resp)) + 1;
+    int resto, array[tamanho];
+    int cont = 0;
+    int numero = 0;
+
+    while(resp > 0) {
+        resto = resp % opcao;
+        array[cont] = resto;
+        resp /= opcao;
+        cont++;
+    }
+
+    for(int i = cont - 1; i >= 0; i--) {
+        numero = numero * 10 + array[i];
+    }
+
+    return numero;
+}
+
 
 int main(){
-printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:35)\n1.a(Não feito)\n1.b(Não feito)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
+printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:55)\n1.a(11:55)\n1.b(Não feito)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
   
   printf("\n\n----CALCULADORA DE CONVERSÃO----\n\nEscolha uma opção:\n(1)Base 10 --> Base 2\n(2)Base 10 --> base 8\n(3)Base 10 --> base 16\n(4)Base 10 --> Código BCD\n(5)Base 10 --> Complemento a 2\n(6)Decimal --> Float e Double\n\n");
 
@@ -10,7 +33,8 @@ printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:35)\n1.a(Não feito
   scanf("%d",&opt);
 
   if(opt==1){
-    
+    scanf("%d",&n);
+    printf("%d",conv(n, 2));
   }
 
   else if(opt==2){
