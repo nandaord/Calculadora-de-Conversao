95ed9e7ca044032ea26579df7548474810de1f95
Maria Fernanda Ordonho <142419456+nandaord@users.noreply.github.com>
2024-08-28 12:08:15 -0300

    Update calculadora.c
    commit para adicionar a função de passar números de base 10 para base 8

--- calculadora.c
+++ calculadora.c
@@ -24,7 +24,7 @@ int conv(int resp, int opcao) {
 
 
 int main(){
-printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:55)\n1.a(11:55)\n1.b(Não feito)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
+printf("Maria Fernanda Suruagy Ordonho - 3A (Última atualização = 28/08/2024 - 12:07)\n1.a(11:55)\n1.b(12:07)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
   
   printf("\n\n----CALCULADORA DE CONVERSÃO----\n\nEscolha uma opção:\n(1)Base 10 --> Base 2\n(2)Base 10 --> base 8\n(3)Base 10 --> base 16\n(4)Base 10 --> Código BCD\n(5)Base 10 --> Complemento a 2\n(6)Decimal --> Float e Double\n\n");
 
@@ -33,16 +33,20 @@ printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:55)\n1.a(11:55)\n1.
   scanf("%d",&opt);
 
   if(opt==1){
+    printf("Digite um número: ");
     scanf("%d",&n);
-    printf("%d",conv(n, 2));
+    printf("%d em base2 = %d",n,conv(n, 2));
   }
 
   else if(opt==2){
-    
+    printf("Digite um número: ");
+    scanf("%d",&n);
+    printf("%d em base8 = %d",n,conv(n, 8));
   }
   else if(opt==3){
-    
+   
   }
+    
   else if(opt==4){
     
   }
