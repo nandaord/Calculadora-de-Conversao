e798a878ae94e58408a53b9ed5de53229a7346af
Maria Fernanda Ordonho <142419456+nandaord@users.noreply.github.com>
2024-08-28 11:49:48 -0300

    Update calculadora.c
    commit pra adicionar o menu e os ifs de opção de entrada

--- calculadora.c
+++ calculadora.c
@@ -2,5 +2,33 @@
 
 int main(){
 printf("Maria Fernanda Suruagy Ordonho - 3A (28/08/2024 - 11:35)\n1.a(Não feito)\n1.b(Não feito)\n1.c(Não feito)\n1.d(Não feito)\n2(Não feito)\n3(Não feito)\n");
+  
+  printf("\n\n----CALCULADORA DE CONVERSÃO----\n\nEscolha uma opção:\n(1)Base 10 --> Base 2\n(2)Base 10 --> base 8\n(3)Base 10 --> base 16\n(4)Base 10 --> Código BCD\n(5)Base 10 --> Complemento a 2\n(6)Decimal --> Float e Double\n\n");
+
+  int opt, n;
+  printf("Opção:");
+  scanf("%d",&opt);
+
+  if(opt==1){
+    
+  }
+
+  else if(opt==2){
+    
+  }
+  else if(opt==3){
+    
+  }
+  else if(opt==4){
+    
+  }
+  else if(opt==5){
+    
+  }
+  else if(opt==6){
+    
+  }
   return 0;
+
+  
 }
