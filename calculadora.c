d659c835dbea203be43408d01712fa8ab3fbdea6
Maria Fernanda Ordonho <142419456+nandaord@users.noreply.github.com>
2024-08-30 18:13:09 -0300

    Update calculadora.c
    commit finalizando os detalhes da funcionalidade de tranformar decimal para binários.
    adicionando a possibilidade da entrada ser um número real.

--- calculadora.c
+++ calculadora.c
@@ -4,14 +4,15 @@
 #include <stdlib.h>
 
 char* conv(int resp, int opcao) {
+
     if (resp == 0) {
-        char* binario = (char*)malloc(2 * sizeof(char));
+        char *binario = (char*)malloc(3 * sizeof(char));
         if (binario == NULL) {
             printf("Erro ao alocar memória!\n");
             return NULL;
         }
-        binario[0] = '0';
-        binario[1] = '\0';
+        strcpy(binario, "0");
+        printf("\nResultado final da parte inteira: 0\n");
         return binario;
     }
 
@@ -20,13 +21,11 @@ char* conv(int resp, int opcao) {
     int cont = 0;
     int resto;
 
-    if (binario == NULL) {
-        printf("Erro ao alocar memória!\n");
-        return NULL;
-    }
-
+    printf("\nCálculo da parte inteira:\n");
+    
     while (resp > 0) {
         resto = resp % opcao;
+        printf("%d %% %d = (%d)\n", resp, opcao, resto);
         binario[cont] = resto + '0';
         resp /= opcao;
         cont++;
@@ -40,7 +39,32 @@ char* conv(int resp, int opcao) {
         binario[cont - 1 - i] = temp;
     }
 
+    printf("\nResultado final da parte inteira: %s\n", binario);
     return binario;
+}
+
+char* convDecimal(double decimal, int n) {
+    
+    char* resultado = (char*)malloc(64 * sizeof(char));
+    if (resultado == NULL) { 
+        printf("Erro ao alocar memória.\n");
+        return NULL;
+    } else {
+    printf("\nCálculo da parte decimal:\n");
+    int i = 1;
+    resultado[0] = '.';
+    while (decimal > 0 && i < 63) {
+        decimal *= n;
+        int bit = (int)decimal;
+        resultado[i++] = bit + '0';
+        decimal -= bit;
+        printf("%.6f (%d)\n", decimal, bit);
+    }
+
+    resultado[i] = '\0';
+    printf("\nResultado final da parte decimal: %s\n", resultado);
+    return resultado;
+}
 }
 
 void convHex(int num) {
@@ -49,6 +73,7 @@ void convHex(int num) {
 
     while (num > 0) {
         hex[cont] = num % 16;
+        printf("Dividindo %d por 16, temos resto %d.\n", num, hex[cont]);
         num = num / 16;
         cont++;
     }
@@ -59,6 +84,7 @@ void convHex(int num) {
             printf("%c", hex[inversor] + 55);
         }
     }
+    printf("\n");
 }
 
 void convBCD(int n) {
@@ -74,6 +100,7 @@ void convBCD(int n) {
 
     for(int i = tam - 1; i >= 0; i--) {
         int digit = temp % 10;
+        printf("Separando dígito: %d\n", digit);
         bcd[i] = digit;
         temp /= 10;
     }
@@ -96,12 +123,14 @@ void convBCD(int n) {
 }
 
 void convComp2(int n){
-  for (int i = 15; i >= 0; i--) {
-      printf("%d", (n >> i) & 1);
-  }
+    printf("Convertendo para binário com complemento a 2...\n");
+    for (int i = 15; i >= 0; i--) {
+        printf("%d", (n >> i) & 1);
+    }
+    printf("\n");
 }
 
-void binariotonotacao(int sinal,const char *binario, char *notacao, int *expoente) {
+void binariotonotacao(int sinal, const char *binario, char *notacao, int *expoente) {
     int comprimento = strlen(binario);
     int i, posicaoPonto = 0;
     int primeiroDigito = 0;
@@ -134,82 +163,87 @@ void binariotonotacao(int sinal,const char *binario, char *notacao, int *expoent
     }
 
     mantissa[23] = '\0';
-    
+
     printf("\nBit de sinal: %d\n", sinal);
     printf("Expoente: %d\n", posicaoPonto);
-    printf("Expoente com viés: %s\n", conv(posicaoPonto+127,2));
+    printf("Expoente com viés: %s\n", conv(posicaoPonto + 127, 2));
     printf("Mantissa: %s\n", mantissa);
-    
 }
 
-int main(){
-printf("Maria Fernanda Suruagy Ordonho - 3A (Última atualização = 30/08/2024-14:49)\n1.a(28/08-11:55)\n1.b(28/08-12:07)\n1.c(28/08-14:25)\n1.d(28/08-14:31)\n2(28/08-14:49)\n3(30/08-15:45)\n");
+int main() {
+    printf("Maria Fernanda Suruagy Ordonho - 3A (Última atualização = 30/08/2024-18:11)\n1.a(30/08-18:12)\n1.b(28/08-12:07)\n1.c(28/08-14:25)\n1.d(28/08-14:31)\n2(28/08-14:49)\n3(30/08-15:45)\n");
 
-  printf("\n\n----CALCULADORA DE CONVERSÃO----\n\nEscolha uma opção:\n(1)Base 10 --> Base 2\n(2)Base 10 --> base 8\n(3)Base 10 --> base 16\n(4)Base 10 --> Código BCD\n(5)Base 10 --> Complemento a 2\n(6)Decimal --> Float e Double\n\n");
+    printf("\n\n----CALCULADORA DE CONVERSÃO----\n\nEscolha uma opção:\n(1) Base 10 --> Base 2\n(2) Base 10 --> Base 8\n(3) Base 10 --> Base 16\n(4) Base 10 --> Código BCD\n(5) Base 10 --> Complemento a 2\n(6) Decimal --> Float e Double\n\n");
 
-  int opt, n;
-  printf("Opção:");
-  scanf("%d",&opt);
+    int opt;
+    double n;
+    printf("Opção: ");
+    scanf("%d", &opt);
 
-  if(opt==1){
-    printf("Digite um número: ");
-    scanf("%d",&n);
-    char* resultado = conv(n,2);
-      if(resultado!=NULL){
-    printf("%d em base2 = %s",n,resultado);
-      }
-    free(resultado);
-  }
+    if(opt == 1) {
+        printf("Digite um número: ");
+        scanf("%lf", &n);
 
-  else if(opt==2){
-    printf("Digite um número: ");
-    scanf("%d",&n);
-      char* resultado = conv(n,8);
-      printf("%d em base2 = %s",n,resultado);
+        if (n<0){
+            printf("Entrada apenas de números positivos!");
+        } 
+        else if (n==0){
+            printf("\n0 em base 2 (binário) = 0");
+        }
+        else {
+        int parteinteira = (int)n;
+        double partedecimal = n-parteinteira;
+
+        char* resultadointeiro = conv(parteinteira, 2);
+        
+        char* resultadodecimal = convDecimal(partedecimal,2);
+        if(resultadointeiro != NULL) {
+            printf("\n%lf em base 2 (binário) = %s%s\n", n, resultadointeiro, resultadodecimal);
+        }
+        free(resultadointeiro);
+        free(resultadodecimal);
+        
+    }}
+    else if(opt == 2) {
+        printf("Digite um número: ");
+        scanf("%lf", &n);
+        char* resultado = conv(n, 8);
+        printf("Resultado da conversão de %lf para base 8: %s\n", n, resultado);
         free(resultado);
-  }
-  else if(opt==3){
-   printf("Digite um número: ");
-        scanf("%d",&n);
-        printf("%d em base16 = ",n);
+        
+    } else if(opt == 3) {
+        printf("Digite um número: ");
+        scanf("%lf", &n);
+        printf("Resultado da conversão de %lf para base 16: ", n);
         convHex(n);
-  }
-
-  else if(opt==4){
-      printf("Digite um número: ");
-      scanf("%d",&n);
-      printf("%d em BCD = ",n);
-      convBCD(n);
-  }
-
-  else if(opt==5){
-      printf("Digite um número: ");
-        scanf("%d",&n);
-        printf("%d em complemento a 2 = ",n);
-        convComp2(n);
-  }
-  else if(opt==6){
-      char notacao[50];
-      int expoente;
-      printf("Digite um número: ");
-      scanf("%d",&n);
-
-      if(n<0){
-          
-          char *binario = conv(n*-1,2);
-          binariotonotacao(1,binario, notacao, &expoente);
-          
-      } else {
-          
-      char *binario = conv(n,2);
-      binariotonotacao(0,binario, notacao, &expoente);
-      
-      }
-
-  } else {
-      printf("Opção inválida!");
-  }
-  return 0;
-
-
+    } else if(opt == 4) {
+        printf("Digite um número: ");
+        scanf("%lf", &n);
+        printf("Resultado da conversão de %lf para código BCD: ", n);
+        convBCD(n);
+    } else if(opt == 5) {
+        printf("Digite um número: ");
+        scanf("%lf", &n);
+        if(n > 0) {
+            printf("Complemento a 2 é um método de calcular apenas os inteiros negativos!\n");
+        } else {
+            printf("Resultado da conversão de %lf para complemento a 2: ", n);
+            convComp2(n);
+        }
+    } else if(opt == 6) {
+        char notacao[50];
+        int expoente;
+        printf("Digite um número: ");
+        scanf("%lf", &n);
+        if(n < 0) {
+            char *binario = conv(n * -1, 2);
+            binariotonotacao(1, binario, notacao, &expoente);
+        } else {
+            char *binario = conv(n, 2);
+            binariotonotacao(0, binario, notacao, &expoente);
+        }
+    } else {
+        printf("Opção inválida!\n");
+    }
+    return 0;
 }
